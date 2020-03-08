90ef6d4c90019153b4aab60d7fab3a31437497c2
Paul Wetzel <wetzelpa@gmail.com>
2020-03-08 16:46:34 -0400

    Updates

---
+++ precache-manifest.caae97bd479277b5f2f66fe62c9ad742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a399c128baf37c068175f31e0855b1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2b13f91fd71044904979",
+    "url": "/monsters-rolodex/static/css/main.ed4bb5c7.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b13f91fd71044904979",
+    "url": "/monsters-rolodex/static/js/main.2427fd9e.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
